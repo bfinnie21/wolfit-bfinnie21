9027be0646c976fe860fcf814203ac1c27ffe37e
Blake Finnie <bfinnie21@mail.wou.edu>
2023-01-18 13:50:35 -0800

    Coverage for a minute ago

--- tests/unit/test_coverage.py
+++ tests/unit/test_coverage.py
@@ -3,3 +3,6 @@ from datetime import datetime, timedelta
 
 def test_seconds_ago():
     assert helpers.less_than_day(30) == "30 seconds ago"
+
+def test_minute_ago():
+    assert helpers.less_than_day(110) == "a minute ago"
