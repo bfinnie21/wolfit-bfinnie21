3d0a66b907cda7edb475d6865164768902171fce
Blake Finnie <bfinnie21@mail.wou.edu>
2023-01-18 14:04:09 -0800

    Coverate for minutes ago

--- tests/unit/test_coverage.py
+++ tests/unit/test_coverage.py
@@ -1,8 +1,18 @@
 from app import helpers
 from datetime import datetime, timedelta
 
+from flask import url_for
+
+import pytest
+
+from app import app, db
+from app.models import ActivityLog, Category, Comment, Post, User
+
 def test_seconds_ago():
-    assert helpers.less_than_day(30) == "30 seconds ago"
+    assert helpers.less_than_day(55) == "55 seconds ago"
 
 def test_minute_ago():
     assert helpers.less_than_day(110) == "a minute ago"
+
+def test_minutes_ago():
+    assert helpers.less_than_day(300) == "5 minutes ago"
