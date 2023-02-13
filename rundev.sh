58900240432c48a383bfbbe45070f30afaeaa66e
Blake Finnie <bfinnie21@mail.wou.edu>
2023-02-13 14:25:25 -0800

    Added evidence folder for hw5

--- rundev.sh
+++ rundev.sh
@@ -2,5 +2,5 @@
 export FLASK_ENV=development
 export FLASK_DEBUG=0
 export WOLFIT_SETTINGS=$(pwd)/dev.settings
-export LOG_URL="http://127.0.0.1:5001"
+export LOG_URL="http://localhost:8080"
 flask run --host=0.0.0.0 --port=8082
